2bed38d80629c2e9bf2176936bb09b82acf58a1e
Jamie Gaida <jamie.gaida.codes@gmail.com>
2022-08-12 16:08:19 +1000

    styler::style_package()

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,13 @@ library(testthat)
 library(AzurePipelineR)
 
 if (.Platform$OS.type == "unix" &&
-    !interactive()) {
+  !interactive()) {
   test_check("AzurePipelineR",
-             reporter = MultiReporter$new(reporters = list(
-               JunitReporter$new(file = "test-results.xml"),
-               CheckReporter$new()
-             )))
+    reporter = MultiReporter$new(reporters = list(
+      JunitReporter$new(file = "test-results.xml"),
+      CheckReporter$new()
+    ))
+  )
 } else {
   test_check("AzurePipelineR")
 }
